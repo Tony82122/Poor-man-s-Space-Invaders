640fe436024e6d3d9bc919260591a6a5b65ef448
Tony82122 <143779031+Tony82122@users.noreply.github.com>
2024-11-09 17:23:18 +0100

    hello

--- Settings/Settings.py
+++ Settings/Settings.py
@@ -1,4 +1,5 @@
-﻿
+﻿import pygame
+
 class Settings:
     def __init__(self):
         """ initialising the game settings"""
---
+++ Ship/Ship.py
@@ -0,0 +1,17 @@
+﻿import pygame
+
+class Ship:
+    def __init__(self,ai_game):
+        self.screen = ai_game.screen
+        self.screen_rect =ai_game.screen.get_rect()
+
+        # Load the ship image and get rect
+
+        self.image = pygame.image.load('images/ship/bmp')
+        self.rect = self.image.get_rect()
+
+        #Start each game with a new ship, bottom centre of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        def blitme(self):
+            self.screen.blit(self.image,self.rect)
